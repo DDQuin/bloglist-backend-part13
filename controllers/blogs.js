bde5ff25363bda9214bbf8020354ec431acbd65e
DDQuin <ddquin420@gmail.com>
2022-07-23 12:35:01 +0100

    ex 13.3

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 
 const e = require('express')
 const { Blog, User } = require('../models')
+const { Op } = require('sequelize')
 const { SECRET } = require('../util/config')
 const jwt = require('jsonwebtoken');
 
@@ -26,12 +27,19 @@ const blogFinder = async (req, res, next) => {
 
 
 router.get('/', async (req, res) => {
+  const where = {}
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
     res.json(blogs)
   })
