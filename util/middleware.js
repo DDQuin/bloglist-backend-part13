76938f5fa2be0ad3afe2a89b2b3f4563cdceaf8c
DDQuin <ddquin420@gmail.com>
2022-07-23 11:33:20 +0100

    ex 13.9

--- util/middleware.js
+++ util/middleware.js
@@ -4,7 +4,7 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'SequelizeValidationError') {
-      return response.status(400).json({ error: error.message })
+      return response.status(400).json({ error: error.message})
     } else if (error.name === 'SequelizeDatabaseError') {
         return response.status(400).json({ error: error.message })
     } else if (error.name === 'TypeError') {
